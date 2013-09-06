1285bf7d347945690596a72f9affbf43acf29bfd
peter-svintsitskyi <peter.svintsitskiy@gmail.com>
2013-09-06 15:34:18 +0300

    Update JWE_Test.php
    Fixed pass $this in scope of anonymous function

--- test/JOSE/JWE_Test.php
+++ test/JOSE/JWE_Test.php
@@ -83,8 +83,9 @@ class JOSE_JWE_Test extends JOSE_TestCase {
     function testDecryptRSA15_A128CBCHS256_WithKeyAsCallback() {
         $input = 'eyJhbGciOiJSU0ExXzUiLCJlbmMiOiJBMTI4Q0JDK0hTMjU2In0.gOIfTaAkLJYGsK-anmDgxokNit2UqKiraKyExUxM0oj5mw2UngEUGvLK-iztMTiONovqwsMmxOsoZLt_t7paCAx1_3KB1YuCZtBF-0_eH54j0KRdF1Ht8xDaPg0nNmkfSqn19EM-fZVDNBK4Jig-8eF0nbtq1EjL9m6AXV1utIQgM5-3gDtnXkNJ8pYkS22Lga4906smr5IkswdlJEvu81GFV7haFb1Edpyw_Ty0El8KW-p0Udz5FFZD_16qO4FzvSJk73X2l21zXENqUXhiFJDXacBOozpyGL0Rf-idwk9-X3mh8RThutcTelVUOWYdcW-7B8oLaeLEPFYeaLLsjQ.AaxiImKsfoBGoM5s9bp90Q.5KBllDM4n5Po3BhQ8CkpTQ.MNpTRLD3plIxs6JqR6h2ww0D97T5R9oNtE7uplkUcdE';
         $jwe = new JOSE_JWE($input);
-        $jwe->decrypt(function($kid){
-            $this->rsa_keys['private'];
+        $self = $this;
+        $jwe->decrypt(function($kid) use($self){
+            $self->rsa_keys['private'];
         });
         $this->assertEquals($this->plain_text, $jwe->plain_text);
     }
