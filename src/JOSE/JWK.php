787805ec6edaaa31db72aace70ab3d4b1544bb07
Benjamin Sonntag <benjamin@octopuce.fr>
2015-12-04 02:00:21 +0100

    adding JWK Thumbprint as of RFC7638 https://tools.ietf.org/html/rfc7638

--- src/JOSE/JWK.php
+++ src/JOSE/JWK.php
@@ -2,6 +2,7 @@
 
 use phpseclib\Crypt\RSA;
 use phpseclib\Math\BigInteger;
+use phpseclib\Crypt\Hash;
 
 class JOSE_JWK {
     var $components = array();
@@ -41,24 +42,46 @@ class JOSE_JWK {
     static function encode($key, $extra_components = array()) {
         switch(get_class($key)) {
             case 'phpseclib\Crypt\RSA':
-                $components = array(
-                    'kty' => 'RSA',
-                    'e' => JOSE_URLSafeBase64::encode($key->publicExponent->toBytes()),
-                    'n' => JOSE_URLSafeBase64::encode($key->modulus->toBytes())
-                );
-                if ($key->exponent != $key->publicExponent) {
-                    $components = array_merge($components, array(
-                        'd' => JOSE_URLSafeBase64::encode($key->exponent->toBytes())
-                    ));
-                }
-                return new self(array_merge($components, $extra_components));
-            default:
-                throw new JOSE_Exception_UnexpectedAlgorithm('Unknown key type');
+	      $components = array(
+				  'kty' => 'RSA',
+				  'e' => JOSE_URLSafeBase64::encode($key->publicExponent->toBytes()),
+				  'n' => JOSE_URLSafeBase64::encode($key->modulus->toBytes())
+				  );
+	      if ($key->exponent != $key->publicExponent) {
+		$components = array_merge($components, array(
+							     'd' => JOSE_URLSafeBase64::encode($key->exponent->toBytes())
+							     ));
+	      }
+	      return new self(array_merge($components, $extra_components));
+	default:
+	  throw new JOSE_Exception_UnexpectedAlgorithm('Unknown key type');
         }
     }
 
     static function decode($components) {
-        $jwk = new self($components);
-        return $jwk->toKey();
+      $jwk = new self($components);
+      return $jwk->toKey();
     }
+    
+    /** 
+     * Returns the JWK Thumbprint of the Json Web Key
+     * see https://tools.ietf.org/html/rfc7638
+     */
+    function thumbprint() {
+      $requiredcomp=array();
+      switch ($this->components['kty']) {
+        case 'RSA':
+	  $requiredcomp=array("e"=>$this->components["e"],
+			      "kty"=>"RSA",
+			      "n"=>$this->components["n"]
+			      ); // ORDER MATTERS as required by RFC !
+        default:
+	  throw new JOSE_Exception_UnexpectedAlgorithm('Unknown key type');
+      }     
+      $hash = new Hash('sha256');
+      return JOSE_URLSafeBase64::encode(
+					$hash->hash( json_encode( $requiredcomp ) )
+					);
+    }
+    
 }
